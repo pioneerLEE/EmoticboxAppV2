00865e759907000c01795c437c8981a06416f8a5
pioneerLee <kde740998@gmail.com>
2019-10-22 14:43:47 +0900

    newsection design

--- screens/Hot.js
+++ screens/Hot.js
@@ -4,7 +4,10 @@ import {
   View,
   Text,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+  Image
 } from 'react-native';
 const {width,height}=Dimensions.get('window');
 
@@ -13,6 +16,20 @@ class Hot extends React.Component {
   render() {
     return (
         <View style={styles.container}>
+          <ScrollView>
+            <View style={{height:120,flexDirection:'row',alignItems:'center'}}>
+              <Image source={{uri: 'https://i.pinimg.com/originals/ac/95/2e/ac952e8833ff7210249b675456e58f37.gif'}}
+               style={{width:90,height:90,backgroundColor:'gray',marginLeft:20}}/>
+              <View style={{height:90}}>
+                <Text style={{marginLeft:20,marginTop:10,fontSize:20}}>오버액션토끼</Text>
+                <Text style={{marginLeft:20,marginTop:5,fontSize:15,color:'gray'}}>애소</Text>
+                <View style={{marginLeft:20,marginTop:15,height:25,width:50,borderColor:'orange',borderRadius:20,borderWidth:1,justifyContent:'center',alignItems:'center'}}>
+                  <Text style={{fontSize:12, color:'#FF3F00'}}>$0.9</Text>
+                </View>
+                <View style={{marginLeft:10,marginTop:15,width:width-130,height:1,backgroundColor:'#f1f3f5'}}/>
+              </View>
+            </View>
+          </ScrollView>
             
         </View>
         );
